84bf31048a0db93ec676fd8b5cd110f4c7f6dcfe
yoel <67129044+yoel0@users.noreply.github.com>
2020-12-22 01:04:57 -0800

    [add] file structure and notes

---
+++ script.js
@@ -0,0 +1,41 @@
+/**************************************************
+ * Three.js Intro * Cultivate Yakima  🪄 🧙🏼‍♂️ 🪄     *
+ * ************************************************
+ * What is Three.js?                              *
+ * Three.js is a powerful Javascript Library for  *
+ * creating 3D Graphics within the Web Browser.   *
+ * Created by Ricardo Cabello aka Mr. Doob using  *
+ * WebGL.                                         *
+ * Link to Mr.Doobs Github(Nirvana)⬇             ❤️
+ * https://github.com/mrdoob                      *
+ *                                                *
+ * WebGL Stands for Web Graphics Library. WebGL   *
+ * is a Javascript API that allows the browser    *
+ * to 'Render' 3D Graphics without the use of any *
+ * plug-ins. WebGL also allows GPU (Graphics      *
+ * Processing Unit) accelerated rendering.        *
+ * What does this mean? it is lean, mean and one  *
+ * delicious little thing.                        *
+ *                                                *
+ * Why should we be interested in using Three.js? *
+ * In a world where technology continually grows  *
+ * and embeds itself in everday life, In a world  *
+ * where users are constantly seeking immersive   *
+ * experiences, the onus is on us as Devs to turn *
+ * dreams in to reality. I am of the opinion that *
+ * the biggest illusion we've ever been fed is    *
+ * that code is black and white. Code is every    *
+ * single color you could ever fathom.            *
+ * ************************************************
+ * Three.js Documentation [Docs]                  *
+ * ************************************************
+ * https://threejs.org/docs/                      *
+ * ************************************************
+ * Demo Examples for Visualization                *
+ * ************************************************
+ * 1. https://github.com/home                     *
+ * 2. http://globe.chromeexperiments.com/         *
+ * 3. https://github.com/dataarts/webgl-globe     *
+ * 4. https://www.m-trust.co.jp/                  *
+ ****************************************************/
+
